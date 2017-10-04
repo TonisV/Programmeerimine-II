17b8fcce4017293ee54804956019b6714e0e2491
Tõnis Vaimann <tonisv@tlu.ee>
2017-10-04 18:19:06 +0300

    Lisatud esimesed vaated.

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const path = require('path');
 
+// Init App
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Home Route
 app.get('/', function(req, res){
-    res.send('THIS S**T IS WORKING! :)');
+    res.render('index');
 });
 
+// Add Route
+app.get('/articles/add', function(req, res) {
+    res.render('add_article');
+});
+
+// Start Server
 app.listen(3000, function(){
     console.log('Server started on port 3000...');
 });
