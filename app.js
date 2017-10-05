b722623945266bf961221b156f47c49d448bee76
Tõnis Vaimann <tonisv@tlu.ee>
2017-10-05 12:28:40 +0300

    Täiendatud vaateid ja app.js. Header ja footer eraldi.

--- app.js
+++ app.js
@@ -10,12 +10,37 @@ app.set('view engine', 'ejs');
 
 // Home Route
 app.get('/', function(req, res){
-    res.render('index');
+    let articles = [
+        {
+            id: 1,
+            title: 'Article One',
+            author: 'Tõnis Vaimann',
+            body: 'This is article one'
+        },
+        {
+            id: 2,
+            title: 'Article Two',
+            author: 'Tõnis Vaimann',
+            body: 'This is article Two'
+        },
+        {
+            id: 3,
+            title: 'Article Three',
+            author: 'Tõnis Vaimann',
+            body: 'This is article Three'
+        }
+    ];
+    res.render('index', {
+        pageTitle: 'Articles',
+        articles: articles
+    });
 });
 
 // Add Route
 app.get('/articles/add', function(req, res) {
-    res.render('add_article');
+    res.render('add_article', {
+        pageTitle: 'Add Article'
+    });
 });
 
 // Start Server
