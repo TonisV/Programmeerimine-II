c8ca84cb4db932d67e7fd9eca4188a3106ae176b
Tõnis Vaimann <tonisv@tlu.ee>
2017-11-03 16:29:37 +0200

    Lisatud artiklite lisamis ja muutmis loogika. Muudetud vaadete välimust.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -25,6 +26,15 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Home Route
 app.get('/', function(req, res){
     Article.find({}, function(err, articles){
@@ -38,9 +48,63 @@ app.get('/', function(req, res){
     });
 });
 
+// Get Single Article
+app.get('/article/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('article', {
+            article: article
+        });
+    });
+})
+
 // Add Route
 app.get('/articles/add', function(req, res) {
     res.render('add_article', {});
+});
+
+// Add Submit POST Route
+app.post('/articles/add', function(req, res) {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.content;
+
+    article.save(function(err){
+      if(err) {
+          console.log(err);
+          return;
+      } else {
+          res.redirect('/');
+      }
+    });
+});
+
+// Load Edit Form
+app.get('/article/edit/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('edit_article', {
+            article: article
+        });
+    });
+})
+
+// Update Submit POST Route
+app.post('/articles/edit/:id', function(req, res) {
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.content;
+
+    let query = {_id:req.params.id};
+
+    Article.update(query, article, function(err){
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 // Start Server
